da5ba87095c4d81b4460f9be0af59da161e21d9e
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-09-12 23:18:18 +0300

    add static home view

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import http from "http";
 
+import fs from "fs/promises";
+import siteCss from "./content/styles/site.css.js";
+import homeHtml from "./views/home.html.js";
+
 const server = http.createServer((req, res) => {
-    res.write("test");
+    
+    if (req.url === "/content/styles/site.css") {
+        res.writeHead(200, {
+            "content-type": "text/css"
+        })
+
+        res.write(siteCss);
+        res.end();
+        return;
+    }
+
+    
+    switch (req.url) {
+        case "/": res.write(homeHtml);
+            break;
+    }
 
     res.end();
 })
+
+
+function readFile(path) {
+    return fs.readFile(path, { encoding: "utf-8"});
+}
+
+async function homeView() {
+
+    const html = readFile("./src/views/home.html");
+    return html;
+}
 
 server.listen(5000, () => console.log("Server is listening on http://localhost:5000"))
